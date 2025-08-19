a6d72fc8597cad040e6cba8d8d1fc01480851121
AndysJN <andrewsjn@gmail.com>
2025-08-19 14:46:31 +0200

    Idea Inicial de la estructura del juego

--- ANeville.cpp
+++ ANeville.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <conio2.h>
-using namespace std;
+#include <windows.h>
+
+/**
+
+Crear Clase Base
+
+Derivar Jugador, Enemigo y Proyectil de Clase Base
+
+Crear una configuracion para la pantalla
+
+Crear una clase para el framework del juego (Puntaje, manejo de enemigos)
+
+**/
 
 int main(int argc, char *argv[]) {
 	
